c70c4c90f0a15ef800cabc4e52df5a4b62085a0e
JeremyTCD <jeremytanchongdao@gmail.com>
2018-02-08 15:57:52 +0800

    Added heading renderer

---
+++ JeremyTCD.DocFx.Plugins.MimoMarkdown/HeadingRenderer.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.DocAsCode.Dfm;
+using Microsoft.DocAsCode.MarkdownLite;
+
+namespace JeremyTCD.DocFx.Plugins.MimoMarkdown
+{
+    public class HeadingRenderer : DfmCustomizedRendererPartBase<IMarkdownRenderer, MarkdownHeadingBlockToken, MarkdownBlockContext>
+    {
+        public override string Name => "AlertRenderer";
+
+        public override bool Match(IMarkdownRenderer renderer, MarkdownHeadingBlockToken token, MarkdownBlockContext context)
+        {
+            // h1 and h2 elements need wrappers for their "copy link" buttons
+            return token.Depth <= 2;
+        }
+
+        public override StringBuffer Render(IMarkdownRenderer renderer, MarkdownHeadingBlockToken token, MarkdownBlockContext context)
+        {
+            string level = token.Depth.ToString();
+            StringBuffer result = $"<div class=\"heading-{level}\">\n";
+            result += "<div class=\"wrapper\">\n";
+            result += $"<h{level} id=\"{renderer.Options.HeaderPrefix}{token.Id}\">\n";
+            foreach (var item in token.Content.Tokens)
+            {
+                result += renderer.Render(item);
+            }
+            result += $"\n</h{level}>\n";
+            result += $"<div data-clipboard-text=\"#{token.Id}\" data-clipboard-action=\"copy\" role=\"button\" title=\"Link copied\">\n"; // data-clipboard-action used by clipboard.js. title used by tippy.js
+            result += "<svg>\n";
+            result += "<use xlink:href=\"#material-design-link\"></use>\n";
+            result += "</svg>\n";
+            result += "</div>\n"; // button
+            result += "</div>\n"; // wrapper
+            result += "</div>\n"; // heading
+
+            return result;
+        }
+    }
+}
