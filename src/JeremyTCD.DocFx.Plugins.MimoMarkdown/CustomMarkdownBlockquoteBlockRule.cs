049dd94e93f2c35362883f4e80600b5117bb3c44
JeremyTCD <jeremytanchongdao@gmail.com>
2018-02-09 15:43:50 +0800

    Made changes to CustomMarkdownBlockquoteBlockRule
    - Now uses the new matcher instead of regex
    - Now sets the context while processing nested tokens

--- src/JeremyTCD.DocFx.Plugins.MimoMarkdown/CustomMarkdownBlockquoteBlockRule.cs
+++ src/JeremyTCD.DocFx.Plugins.MimoMarkdown/CustomMarkdownBlockquoteBlockRule.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.DocAsCode.MarkdownLite;
+using Microsoft.DocAsCode.MarkdownLite.Matchers;
 using System;
 using System.Collections.Immutable;
 using System.Text.RegularExpressions;
@@ -16,23 +17,58 @@ namespace JeremyTCD.DocFx.Plugins.MimoMarkdown
 
         public virtual Regex LeadingBlockquote => new Regex(@"^ *> ?", RegexOptions.Multiline | RegexOptions.Compiled, TimeSpan.FromSeconds(10));
 
+        private static readonly Matcher _BlockquoteMatcher =
+            (
+                // @" *> *\n"
+                (Matcher.WhiteSpacesOrEmpty + '>' + Matcher.WhiteSpacesOrEmpty + (Matcher.NewLine | Matcher.EndOfString)) |
+                (
+                    // @" *>[^\n]+(\n[^\n]+)*"
+                    Matcher.WhiteSpacesOrEmpty + '>' + Matcher.AnyStringInSingleLine +
+                    (
+                        Matcher.NewLine +
+                        // expect following:
+                        (
+                            // heading
+                            (Matcher.WhiteSpacesOrEmpty + Matcher.Char('#').Repeat(1, 6) + Matcher.WhiteSpaces + Matcher.AnyStringInSingleLine + '\n') |
+                            // hr
+                            ((Matcher.WhiteSpacesOrEmpty + Matcher.AnyCharIn('-', '*', '_')).RepeatAtLeast(3) + Matcher.WhiteSpacesOrEmpty + '\n') |
+                            // list
+                            (Matcher.WhiteSpacesOrEmpty + Matcher.AnyCharIn('-', '*') + Matcher.WhiteSpaces + Matcher.AnyStringInSingleLine + '\n') |
+                            (Matcher.WhiteSpacesOrEmpty + Matcher.AnyCharInRange('0', '9').RepeatAtLeast(1) + '.' + Matcher.WhiteSpaces + Matcher.AnyStringInSingleLine + '\n') |
+                            // @" *>"
+                            (Matcher.WhiteSpacesOrEmpty + '>')
+                        ).ToNegativeTest() +
+                        Matcher.AnyStringInSingleLine
+                    ).RepeatAtLeast(0) +
+                    (Matcher.NewLine | Matcher.EndOfString)
+                )
+            ).RepeatAtLeast(1) +
+            Matcher.NewLine.RepeatAtLeast(0);
+
         public virtual IMarkdownToken TryMatch(IMarkdownParser parser, IMarkdownParsingContext context)
         {
-            Match match = Blockquote.Match(context.CurrentMarkdown);
-            if (match.Length == 0)
-            {
-                return null;
-            }
-            SourceInfo sourceInfo = context.Consume(match.Length);
-            string capStr = LeadingBlockquote.Replace(sourceInfo.Markdown, string.Empty);
-            ImmutableArray<IMarkdownToken> blockTokens = parser.Tokenize(sourceInfo.Copy(capStr));
-            blockTokens = TokenHelper.CreateParagraghs(parser, this, blockTokens, true, sourceInfo);
+            MarkdownBlockquoteBlockToken result = null;
 
-            return new MarkdownBlockquoteBlockToken(
-                this,
-                parser.Context,
-                blockTokens,
-                sourceInfo);
+            // Need to switch context (see DfmBlockquoteBlockRule.cs)
+            IMarkdownContext initialContext = parser.SwitchContext(MarkdownBlockContext.IsBlockQuote, true);
+            MatchResult match = context.Match(_BlockquoteMatcher);
+
+            if (match?.Length > 0)
+            {
+                SourceInfo sourceInfo = context.Consume(match.Length);
+                string capStr = LeadingBlockquote.Replace(sourceInfo.Markdown, string.Empty);
+                ImmutableArray<IMarkdownToken> blockTokens = parser.Tokenize(sourceInfo.Copy(capStr));
+                blockTokens = TokenHelper.CreateParagraghs(parser, this, blockTokens, true, sourceInfo);
+                result = new MarkdownBlockquoteBlockToken(
+                    this,
+                    parser.Context,
+                    blockTokens,
+                    sourceInfo);
+            }
+
+            parser.SwitchContext(initialContext);
+
+            return result;
         }
     }
 }
