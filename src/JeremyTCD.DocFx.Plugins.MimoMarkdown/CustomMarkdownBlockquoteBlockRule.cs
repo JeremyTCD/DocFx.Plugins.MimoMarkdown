c778bfd3c6c71b91a782aed7a59e01dbb1f711eb
JeremyTCD <jeremytanchongdao@gmail.com>
2018-02-09 12:19:02 +0800

    Added custom MarkdownBlockquoteBlockRule

---
+++ src/JeremyTCD.DocFx.Plugins.MimoMarkdown/CustomMarkdownBlockquoteBlockRule.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.DocAsCode.MarkdownLite;
+using System;
+using System.Collections.Immutable;
+using System.Text.RegularExpressions;
+
+namespace JeremyTCD.DocFx.Plugins.MimoMarkdown
+{
+    /// <summary>
+    /// Custom rule to get around consecutive blockquotes not working: https://github.com/dotnet/docfx/issues/2424
+    /// </summary>
+    public class CustomMarkdownBlockquoteBlockRule : IMarkdownRule
+    {
+        public virtual string Name => "CustomBlockquote";
+
+        public virtual Regex Blockquote => new Regex(@"^( *>[^\n]+(\n(?! *\[([^\]]+)\]: *<?([^\s>]+)>?(?: +[""(]([^\n]+)["")])? *(?:\n+|$))[^\n]+)*\n)+\n*", RegexOptions.Compiled, TimeSpan.FromSeconds(10));
+
+        public virtual Regex LeadingBlockquote => new Regex(@"^ *> ?", RegexOptions.Multiline | RegexOptions.Compiled, TimeSpan.FromSeconds(10));
+
+        public virtual IMarkdownToken TryMatch(IMarkdownParser parser, IMarkdownParsingContext context)
+        {
+            Match match = Blockquote.Match(context.CurrentMarkdown);
+            if (match.Length == 0)
+            {
+                return null;
+            }
+            SourceInfo sourceInfo = context.Consume(match.Length);
+            string capStr = LeadingBlockquote.Replace(sourceInfo.Markdown, string.Empty);
+            ImmutableArray<IMarkdownToken> blockTokens = parser.Tokenize(sourceInfo.Copy(capStr));
+            blockTokens = TokenHelper.CreateParagraghs(parser, this, blockTokens, true, sourceInfo);
+
+            return new MarkdownBlockquoteBlockToken(
+                this,
+                parser.Context,
+                blockTokens,
+                sourceInfo);
+        }
+    }
+}
--- src/JeremyTCD.DocFx.Plugins.MimoMarkdown/MimoEngineCustomizer.cs
+++ src/JeremyTCD.DocFx.Plugins.MimoMarkdown/MimoEngineCustomizer.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.DocAsCode.Dfm;
+using Microsoft.DocAsCode.MarkdownLite;
 using System.Collections.Generic;
 using System.Composition;
 
@@ -10,6 +11,9 @@ namespace JeremyTCD.DocFx.Plugins.MimoMarkdown
         public void Customize(DfmEngineBuilder builder, IReadOnlyDictionary<string, object> parameters)
         {
             builder.BlockRules = builder.BlockRules.Insert(0, new AlertRule());
+
+            int blockquoteRuleIndex = builder.BlockRules.FindIndex(r => r is MarkdownBlockquoteBlockRule);
+            builder.BlockRules = builder.BlockRules.Insert(blockquoteRuleIndex, new CustomMarkdownBlockquoteBlockRule());
         }
     }
 }
