312c2bd4cc5186a1c92f90d7c37c4ed9bfae0bab
JeremyTCD <jeremytanchongdao@gmail.com>
2018-04-24 21:56:53 +0800

    Added CollapseLength option to ClippingArea

--- src/JeremyTCD.DocFx.Plugins.MimoMarkdown/Shared/ClippingArea.cs
+++ src/JeremyTCD.DocFx.Plugins.MimoMarkdown/Shared/ClippingArea.cs
@@ -4,6 +4,8 @@
     {
         public int DedentLength { get; set; } = -1;
 
+        public int CollapseLength { get; set; } = -1;
+
         public string Before { get; set; } = null;
 
         public string After { get; set; } = null;
