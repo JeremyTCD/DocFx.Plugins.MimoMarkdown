7f5b09ec564150b958198c031fe08bf437b0c29c
JeremyTCD <jeremytanchongdao@gmail.com>
2018-10-03 15:41:00 +0800

    Renamed folders, added MimoMarkdownService and MimoMarkdownServiceProvider

---
+++ src/MimoMarkdown/MimoMarkdownService.cs
@@ -0,0 +1,81 @@
+﻿namespace JeremyTCD.DocFx.Plugins.MimoMarkdown
+{
+    using Markdig;
+    using Markdig.Extensions.AutoIdentifiers;
+    using Microsoft.DocAsCode.Plugins;
+    using System;
+
+    // TODO add roslynator
+    // TODO catch exceptions and log using Logger
+    // TODO clean up exception messages
+    // TODO look through default extensions
+    //  - do we need yamlheaderextension?
+    //  - what other extensions are useful
+    // TODO add lexiblocks
+
+    public class MimoMarkdownService : IMarkdownService
+    {
+        public string Name => "mimo-markdown";
+
+        private readonly MarkdownServiceParameters _parameters;
+
+        public MimoMarkdownService(MarkdownServiceParameters parameters)
+        {
+            _parameters = parameters;
+        }
+
+        public MarkupResult Markup(string src, string path)
+        {
+            return Markup(src, path, false);
+        }
+
+        public MarkupResult Markup(string src, string path, bool enableValidation)
+        {
+            if (src == null)
+            {
+                throw new ArgumentNullException(nameof(src));
+            }
+
+            if (path == null)
+            {
+                throw new ArgumentException("file path can't be null or empty.");
+            }
+
+            MarkdownPipeline markdownPipeline = CreatePipeline();
+
+            return new MarkupResult
+            {
+                Html = Markdown.ToHtml(src, markdownPipeline)
+            };
+        }
+
+        private MarkdownPipeline CreatePipeline()
+        {
+            var builder = new MarkdownPipelineBuilder().
+                // UsePipeTables(). - create custom responsive pipe tables
+                // UseGridTables(). - create custom responsive, free width grid tables
+                UseEmphasisExtras().
+                UseDefinitionLists().
+                UseFootnotes().
+                UseAutoIdentifiers(AutoIdentifierOptions.GitHub | AutoIdentifierOptions.AutoLink).
+                UseAutoLinks().
+                UseTaskLists().
+                UseListExtras().
+                UseMediaLinks().
+                UseAbbreviations().
+                UseFooters().
+                UseFigures().
+                UseCitations().
+                UseCustomContainers().
+                UseGenericAttributes().
+                UseMathematics().
+                UseSmartyPants().
+                UseDiagrams().
+                UseYamlFrontMatter();
+
+
+
+            return builder.Build();
+        }
+    }
+}
---
+++ src/MimoMarkdown/MimoMarkdownServiceProvider.cs
@@ -0,0 +1,21 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace JeremyTCD.DocFx.Plugins.MimoMarkdown
+{
+    using System.Composition;
+
+    using Microsoft.DocAsCode.Plugins;
+
+    [Export("mimo-markdown", typeof(IMarkdownServiceProvider))]
+    public class MarkdigServiceProvider : IMarkdownServiceProvider
+    {
+        [Import]
+        public ICompositionContainer Container { get; set; }
+
+        public IMarkdownService CreateMarkdownService(MarkdownServiceParameters parameters)
+        {
+            return new MimoMarkdownService(parameters);
+        }
+    }
+}
